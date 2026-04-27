6a3d519e9c81704d991a315810616f5a45ab1701
Muhammed Nasser <mo.nasser20032003@gmail.com>
2026-04-27 03:53:03 +0300

    Add fluent validators

--- Resturant/Resturants.Application/Restaurants/Dtos/CreateRestaurantDto.cs
+++ Resturant/Resturants.Application/Restaurants/Dtos/CreateRestaurantDto.cs
@@ -1,18 +1,27 @@
-﻿namespace Restaurants.Application.Restaurants.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Restaurants.Application.Restaurants.Dtos
 {
     public class CreateRestaurantDto
     {
+        //[StringLength(maximumLength: 100 , MinimumLength = 3)]
         public string Name { get; set; } = default!;
         public string Description { get; set; } = default!;
+
+        //[Required(ErrorMessage = "Category is required")]
         public string Category { get; set; } = default!;
         public bool IsOpen { get; set; }
         public bool HasDelivery { get; set; }
 
+        //[EmailAddress(ErrorMessage = "Please Provide a valid Email Address")]
         public string? ContactEmail { get; set; }
+        //[Phone(ErrorMessage = "Please Provide a valid Phone Number")]
         public string? ContactNumber { get; set; }
 
         public string City { get; set; } = default!;
         public string Street { get; set; } = default!;
+
+        //[RegularExpression(@"\d{2}-\d{3}")]
         public string PostalCode { get; set; } = default!;
     }
 }
---
+++ Resturant/Resturants.Application/Restaurants/Validators/CreateRestaurantDtoValidators.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using Restaurants.Application.Restaurants.Dtos;
+
+namespace Restaurants.Application.Restaurants.Validators
+{
+    public class CreateRestaurantDtoValidators : AbstractValidator<CreateRestaurantDto>
+    {
+        private readonly List<string> validCategories = [ "Italian", "Chinese", "Mexican", "Indian", "Fast Food" ];
+        public CreateRestaurantDtoValidators()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Name is required.")
+                .MaximumLength(100).WithMessage("Name cannot exceed 100 characters.")
+                .MinimumLength(3).WithMessage("Name must be at least 3 characters long.");
+
+            RuleFor(x => x.ContactEmail)
+                .EmailAddress().WithMessage("Please provide a valid email address.");
+
+            RuleFor(x => x.PostalCode)
+                .Matches(@"\d{2}-\d{3}").WithMessage("Postal code must be in the format XX-XXX.");
+
+            RuleFor(x => x.Category)
+                .NotEmpty().WithMessage("Category is required.")
+                .Must(category => validCategories.Contains(category))
+                .WithMessage($"Category must be one of the following: {string.Join(", ", validCategories)}");
+        }
+    }
+}
