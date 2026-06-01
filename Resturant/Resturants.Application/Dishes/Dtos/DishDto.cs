aabe2eb525059d960d320254f1e3440c3af6b500
Muhammed Nasser <mo.nasser20032003@gmail.com>
2026-06-01 15:45:22 +0300

    test cicd

--- Resturant/Resturants.Application/Dishes/Dtos/DishDto.cs
+++ Resturant/Resturants.Application/Dishes/Dtos/DishDto.cs
@@ -9,7 +9,20 @@ namespace Restaurants.Application.Dishes.Dtos
         public string Description { get; set; } = default!;
         public decimal Price { get; set; }
         public int? KiloCalories { get; set; }
-         
+
+        //public static DishDto? FromEntity(Dish? dish)
+        //{
+        //    if (dish == null) return null;
+        //    return new DishDto
+        //    {
+        //        Id = dish.Id,
+        //        Name = dish.Name,
+        //        Description = dish.Description,
+        //        Price = dish.Price,
+        //        KiloCalories = dish.KiloCalories
+        //    };
+        //}
+
         //public static DishDto? FromEntity(Dish? dish)
         //{
         //    if (dish == null) return null;
